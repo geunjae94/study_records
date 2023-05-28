437fed29c16720d9c6de2470281e26ac284e95ae
geunjae94 <dlrmswo111111@gmail.com>
2023-05-28 17:22:28 +0900

    comment, refactor: <Level 1 - 신고 결과 받기_2022 KAKAO BLIND RECRUITMENT> 변수명 변경, 메서드 사용 개선 및 주석 추가, 수정

--- coding_test_practice/programmers/Solution7.java
+++ coding_test_practice/programmers/Solution7.java
@@ -6,51 +6,52 @@ import java.util.stream.Stream;
 
 // level 1 신고 결과 받기_2022 KAKAO BLIND RECRUITMENT
 class Solution7 {
-    // 참고: Map<String, List<String>>, new LinkedHashMap<>(), .mapToInt(i -> i.intValue()), .boxed().toArray(Integer[]::new)
+    // 참고: Map<String, List<String>>, new LinkedHashMap<>(), ~~.mapToInt(Integer::intValue), ~~.boxed().toArray(Integer[]::new)
 
     public int[] solution(String[] id_list, String[] report, int k) {
 
-        Map<String, Integer> resultSave = new LinkedHashMap<>(); //배열로 변환전 최종 결과값 저장(유저당 받은 결과 메일 수)
-        Map<String, List<String>> reportedUsersMap = new LinkedHashMap<>(); //신고당한 유저에 대한 신고한 유저의 집합들 ex) {frodo=[muzi, apeach], ...}
-        Map<String, Integer> reportedIdCountMap = new LinkedHashMap<>();//신고당한 유저의 신고당한 횟수 ex) {muzi=1, frodo=2, ...}
+        Map<String, Integer> mailCount = new LinkedHashMap<>(); //유저당 받은 결과 메일 수(배열로 변환전 최종 결과값 저장)
+        Map<String, List<String>> reporters = new LinkedHashMap<>(); //신고당한 유저에 대한 신고한 유저의 집합들 ex) {frodo=[muzi, apeach], ...}
+        Map<String, Integer> reportedIdCount = new LinkedHashMap<>();//신고당한 유저의 신고당한 횟수 ex) {muzi=1, frodo=2, ...}
 
         for (String s : id_list) { //map 초기화: {muzi=0, frodo=0, apeach=0, neo=0}
-            reportedIdCountMap.put(s,0);
-            resultSave.put(s,0);
+            reportedIdCount.put(s,0);
+            mailCount.put(s,0);
         }
 
         for (String s : report) { //key: String, value: List 추가, 해당 key에 대한 값 추가, 신고당한 횟수 증가
             String[] s1 = s.split(" ");
-            if (reportedUsersMap.containsKey(s1[1]) && reportedUsersMap.get(s1[1]).contains(s1[0])) { //report 중복 값 제외
+            if (reporters.containsKey(s1[1]) && reporters.get(s1[1]).contains(s1[0])) { //report 중복 값 제외
                 continue;
-            } else if (reportedUsersMap.containsKey(s1[1]) && !reportedUsersMap.get(s1[1]).contains(s1[0])) { //중복 값 존재 하지 않으면
-                reportedUsersMap.get(s1[1]).add(s1[0]);
-                reportedIdCountMap.put(s1[1], (reportedIdCountMap.get(s1[1])) + 1);
+            } else if (reporters.containsKey(s1[1]) && !reporters.get(s1[1]).contains(s1[0])) { //중복 값 존재 하지 않으면
+                reporters.get(s1[1]).add(s1[0]);
+                reportedIdCount.put(s1[1], (reportedIdCount.get(s1[1])) + 1);
             } else { //최초 값 존재 하지 않으면
                 List<String> list = new ArrayList<>(); //범위 내 for loop시 새 list추가
-                reportedUsersMap.put(s1[1], list);
-                reportedUsersMap.get(s1[1]).add(s1[0]);
-                reportedIdCountMap.put(s1[1], (reportedIdCountMap.get(s1[1])) + 1);
+                reporters.put(s1[1], list);
+                reporters.get(s1[1]).add(s1[0]);
+                reportedIdCount.put(s1[1], (reportedIdCount.get(s1[1])) + 1);
             }
         }
 
-        reportedIdCountMap.keySet().forEach(key-> {
-            int valueToKey = reportedIdCountMap.get(key);
+        reportedIdCount.keySet().forEach(key-> {
+            int valueToKey = reportedIdCount.get(key);
                     if (valueToKey>=k) { //k횟수 이상 신고당한 유저를 기준으로
-                        List<String> listVal = reportedUsersMap.get(key); //해당 유저를 신고한 유저의 집합중
+                        List<String> listVal = reporters.get(key); //해당 유저를 신고한 유저의 집합중
                         listVal.forEach(value->{ //key: 신고한 유저, value: 신고한 유저에 대한 수 추가, 중복은 그 수만큼 증가
-                            resultSave.put(value,resultSave.get(value)+1);
+                            mailCount.put(value, mailCount.get(value)+1);
                         });
                     }
                 }
         );
 
-        /*System.out.println(reportedUsersMap);
-        System.out.println(reportedIdCountMap);
-        System.out.println(resultSave);*/
+        /*System.out.println(reporters);
+        System.out.println(reportedIdCount);
+        System.out.println(mailCount);*/
 
-        int[] answer = resultSave.values().stream().mapToInt(i -> i.intValue()).toArray(); //mapToInt(i -> i.intValue()): Stream<int[]>또는 IntStream일 것
-
+        int[] answer = mailCount.values().stream().mapToInt(Integer::intValue).toArray(); //mapToInt(i -> i.intValue()) -> mapToInt(Integer::intValue)
+                                                                                          //(추측) Integer값->int로 변환 -> int[]에 전부 대입 -> 해당 int[]를 IntStream의 int[]에 대입
+                                                                                          //(추측) 해당 IntStream 반환     *Stream<int[]>
         /*int[] ints = {1, 2, 3};
         IntStream stream = Arrays.stream(ints);
         int[] ints1 = stream.toArray();
