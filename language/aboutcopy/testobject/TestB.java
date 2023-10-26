20d0902b69172b1f1d2f73a490c735f7458ef463
geunjae94 <dlrmswo111111@gmail.com>
2023-10-26 17:44:40 +0900

    [add, test]: Test class에 관한 새 파일 추가

---
+++ language/aboutcopy/testobject/TestB.java
@@ -0,0 +1,13 @@
+package sideproject.personalquestions.test2.aboutcopy.testobject;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TestB {
+    private int i;
+    private String str;
+}
